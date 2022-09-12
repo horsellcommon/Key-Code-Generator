b875d2f12f188179ef7d2f747d72bb3e104a990f
horsellcommon <crawfordj716@gmail.com>
2022-09-12 16:20:22 +0100

    Streamlined code based on feedback and reduced margins/font sizes

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ const conLog = document.addEventListener("keypress", () => {
 })
 
 
-let blankText = document.getElementById("whichKey")
-const textOverwrite = document.addEventListener("keypress", () => {
-    loggedKey = event.which
-    blankText.textContent = loggedKey 
-})
+let blankText = document.getElementById("whichKey");
+let leftText = document.getElementById("whatKey");
+let rightText = document.getElementById("keyCode");
 
-let leftText = document.getElementById("whatKey")
-const keyDisplay = document.addEventListener("keypress", () => {
-    loggedLetter = event.key
-    leftText.textContent = loggedLetter
-})
-
-let rightText = document.getElementById("keyCode")
-const codeDisplay = document.addEventListener("keypress", () => {
-    loggedCode = event.code
-    rightText.textContent = loggedCode
-})
+document.addEventListener("keypress", () => {
+  blankText.textContent = event.which;
+  leftText.textContent = event.key;
+  rightText.textContent = event.code;
+});
